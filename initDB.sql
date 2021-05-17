f9b200519d250d4b43ff413d629e4f60583ab34a
Hanna <74618691+HannaRaus@users.noreply.github.com>
2021-05-17 16:39:16 +0300

    Update initDB.sql
    rename custumer to customer, add enum to skills, add columns to projects, customers, reorganized customers_companies.

--- initDB.sql
+++ initDB.sql
@@ -1,27 +1,28 @@
-CREATE TABLE custumers
-(custumer_id SERIAL PRIMARY KEY,
- first_name VARCHAR(100) NOT NULL,
- last_name VARCHAR(100) NOT NULL);
+CREATE TABLE customers
+(customer_id SERIAL PRIMARY KEY,
+ customer_name VARCHAR(100) NOT NULL,
+ industry VARCHAR(100));
 
 CREATE TABLE companies
 (company_id SERIAL PRIMARY KEY,
-company_name VARCHAR(100) NOT NULL);
-
-CREATE TABLE custumer_companies
-(custumer_id int NOT NULL,
-company_id int NOT NULL,
-FOREIGN KEY (custumer_id) REFERENCES custumers(custumer_id),
-FOREIGN KEY (company_id) REFERENCES companies(company_id),
-UNIQUE (custumer_id, company_id));
+company_name VARCHAR(100) NOT NULL,
+headquarters VARCHAR(100));
 
 CREATE TABLE projects 
 (project_id SERIAL PRIMARY KEY,
-project_name VARCHAR(100)) NOT NULL,
+project_name VARCHAR(100) NOT NULL,
+project_description VARCHAR(100));
+
+CREATE TABLE customers_companies
+(customer_id int NOT NULL,
 company_id int NOT NULL,
-FOREIGN KEY (company_id) REFERENCES companies(company_id);
+project_id int NOT NULL,
+FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+FOREIGN KEY (company_id) REFERENCES companies(company_id),
+FOREIGN KEY (project_id) REFERENCES projects(project_id),
+UNIQUE (customer_id, company_id, project_id));
 
-
-1CREATE TYPE sex AS ENUM ('male', 'female');
+CREATE TYPE sex AS ENUM ('male', 'female');
 CREATE TABLE developers 
 (developer_id SERIAL PRIMARY KEY,
 first_name VARCHAR(100) NOT NULL,
@@ -36,9 +37,10 @@ FOREIGN KEY (developer_id) REFERENCES developers(developer_id),
 UNIQUE (project_id, developer_id));
 
 CREATE TYPE level AS ENUM ('Junior', 'Middle', 'Senior');
+CREATE TYPE language AS ENUM ('Java', 'C++', 'C#', 'JS');
 CREATE TABLE skills
 (skill_id SERIAL PRIMARY KEY,
-branch VARCHAR(100) NOT NULL,
+branch language,
 skill_level level);
 
 CREATE TABLE developer_skills
@@ -47,20 +49,3 @@ developer_id int NOT NULL,
 FOREIGN KEY (skill_id) REFERENCES skills(skill_id),
 FOREIGN KEY (developer_id) REFERENCES developers(developer_id),
 UNIQUE (skill_id, developer_id));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
