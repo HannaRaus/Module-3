3c379b4dc5b4e12fae29a463cbbf39c8dfa5b19f
Hanna <74618691+HannaRaus@users.noreply.github.com>
2021-05-18 11:34:17 +0300

    Update 6.sql

--- 6.sql
+++ 6.sql
@@ -5,7 +5,8 @@ FROM projects p, project_developers pd, developers d
 WHERE p.project_id=pd.project_id AND pd.developer_id=d.developer_id AND p.project_id IN
 (	SELECT p.project_id
 	FROM projects p
-	WHERE cost = (SELECT MIN(cost)	FROM projects)
+ 	GROUP BY p.project_id
+	HAVING cost = (SELECT MIN(cost)	FROM projects)
 	--LIMIT 1 
  )
 GROUP BY p.project_id;
