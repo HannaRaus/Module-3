b4d9393f069a27c36dd88b3b40690dedc2d864bb
Hanna <74618691+HannaRaus@users.noreply.github.com>
2021-05-21 12:32:37 +0300

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -8,6 +8,5 @@ UPDATE projects proj SET cost =
 	FROM projects p
 	INNER JOIN project_developers pd ON p.project_id = pd.project_id
 	INNER JOIN developers d ON d.developer_id=pd.developer_id
- 	GROUP BY p.project_id
-	HAVING p.project_id = proj.project_id
+	WHERE p.project_id = proj.project_id
  );
