c11ba97dcefe4d0fb0721ae7c94995e20b04705f
Hanna <74618691+HannaRaus@users.noreply.github.com>
2021-05-21 12:16:53 +0300

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -4,4 +4,5 @@ SELECT  SUM(d.salary) AS total_cost
 FROM developers d
 INNER JOIN developer_skills ds ON d.developer_id=ds.developer_id
 INNER JOIN skills s ON ds.skill_id=s.skill_id
-WHERE s.branch = 'Java';
+GROUP BY s.branch
+HAVING s.branch = 'Java';
